1b507ff59ef099b9c458b3cd471161a2f78927e7
gouveiarocha <gouveia.rocha@hotmail.com>
2022-03-22 16:04:32 -0300

    add study codes

--- Alura_TestesAutomatizados2/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
+++ Alura_TestesAutomatizados2/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
@@ -1,29 +1,45 @@
 package br.com.alura.leilao.ui.activity;
 
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.assertion.ViewAssertions;
-import androidx.test.espresso.matcher.ViewMatchers;
+import android.content.Intent;
+
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.io.IOException;
+
+import br.com.alura.leilao.api.retrofit.client.LeilaoWebClient;
+import br.com.alura.leilao.model.Leilao;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 public class ListaLeilaoTelaTest {
 
     // instancia da act que será testada
     @Rule
-    public ActivityTestRule activity = new ActivityTestRule(
+    public ActivityTestRule<ListaLeilaoActivity> activity = new ActivityTestRule<>(
             ListaLeilaoActivity.class, // activity
             true, // habilita o modo de toque
             true); // se deseja inicializar a act ou não
 
     @Test
-    public void deve_AparecerUmLeilao_QuandoCarregarUmLeilaoNaAPI() {
+    public void deve_AparecerUmLeilao_QuandoCarregarUmLeilaoNaAPI() throws IOException {
+
+        Leilao leilaoSalvo = new LeilaoWebClient().salva(new Leilao("Carro"));
+        if (leilaoSalvo == null){
+            Assert.fail("Leilão não foi salvo...");
+        }
+
+        activity.launchActivity(new Intent());
 
         // pega um view com caracteristica textual, onde o texto desejado é "Casa" e com o .check
         // verificamos no isDisplayed, se ela esta aparecendo na ui
-        Espresso.onView(ViewMatchers.withText("Casa"))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withText("Carro")).check(matches(isDisplayed()));
 
     }
 
