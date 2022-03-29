64908af2660f25d2afa3c4b43ba0c874fbf57a47
gouveiarocha <gouveia.rocha@hotmail.com>
2022-03-29 17:33:09 -0300

    Training completion Automated Tests Android

--- Alura_TestesAutomatizados2/src/androidTest/java/br/com/alura/leilao/BaseTesteIntegracao.java
+++ Alura_TestesAutomatizados2/src/androidTest/java/br/com/alura/leilao/BaseTesteIntegracao.java
@@ -9,7 +9,22 @@ import org.junit.Assert;
 import java.io.IOException;
 
 import br.com.alura.leilao.api.retrofit.client.TesteWebClient;
+import br.com.alura.leilao.database.dao.UsuarioDAO;
 import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
 
 public abstract class BaseTesteIntegracao {
 
@@ -18,11 +33,25 @@ public abstract class BaseTesteIntegracao {
 
     private final TesteWebClient webClient = new TesteWebClient();
 
-    protected void limpaBaseDeDadosDaAPI() throws IOException {
-        boolean bancoDeDadosNaoFoiLimpo = !webClient.limpaBancoDeDados();
-        if (bancoDeDadosNaoFoiLimpo) {
-            Assert.fail(ERRO_LIMPEZA_DADOS_API);
-        }
+    protected void propoeNovoLance(String valorLance, int idUsuario, String nomeUsuario) {
+
+        onView(allOf(withId(R.id.lances_leilao_fab_adiciona), isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withText("Novo lance"), withId(R.id.alertTitle)))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(withId(R.id.form_usuario_nome_edittext), isDisplayed()))
+                .perform(click(), typeText(valorLance), closeSoftKeyboard());
+
+        onView(allOf(withId(R.id.form_lance_usuario), isDisplayed()))
+                .perform(click());
+        onData(is(new Usuario(idUsuario, nomeUsuario))).inRoot(isPlatformPopup())
+                .perform(click());
+
+        onView(allOf(withText("Propor"), isDisplayed()))
+                .perform(click());
+
     }
 
     protected void tentaSalvarLeilaoNaAPI(Leilao... leiloes) throws IOException {
@@ -31,6 +60,23 @@ public abstract class BaseTesteIntegracao {
             if (leilaoSalvo == null) {
                 Assert.fail(ERRO_LEILÃO_NÃO_FOI_SALVO + leilao.getDescricao());
             }
+        }
+    }
+
+    protected void tentaSalvarUsuariosNoBancoDeDados(Usuario... usuarios) {
+        UsuarioDAO dao = new UsuarioDAO(InstrumentationRegistry.getTargetContext());
+        for (Usuario usuario : usuarios) {
+            Usuario usuarioSalvo = dao.salva(usuario);
+            if (usuarioSalvo == null) {
+                Assert.fail("Usuário " + usuario + " não foi salvo...");
+            }
+        }
+    }
+
+    protected void limpaBaseDeDadosDaAPI() throws IOException {
+        boolean bancoDeDadosNaoFoiLimpo = !webClient.limpaBancoDeDados();
+        if (bancoDeDadosNaoFoiLimpo) {
+            Assert.fail(ERRO_LIMPEZA_DADOS_API);
         }
     }
 
--- Alura_TestesAutomatizados2/src/androidTest/java/br/com/alura/leilao/ui/activity/LancesLeilaoTelaTeste.java
+++ Alura_TestesAutomatizados2/src/androidTest/java/br/com/alura/leilao/ui/activity/LancesLeilaoTelaTeste.java
@@ -2,8 +2,6 @@ package br.com.alura.leilao.ui.activity;
 
 import android.content.Intent;
 
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.After;
@@ -15,15 +13,26 @@ import java.io.IOException;
 
 import br.com.alura.leilao.BaseTesteIntegracao;
 import br.com.alura.leilao.R;
+import br.com.alura.leilao.formatter.FormatadorDeMoeda;
 import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.contrib.RecyclerViewActions.*;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
 
 public class LancesLeilaoTelaTeste extends BaseTesteIntegracao {
 
@@ -64,8 +73,147 @@ public class LancesLeilaoTelaTeste extends BaseTesteIntegracao {
         onView(withText("Cadastrar usuário")).perform(click());
 
         // clica no fab tela de lista de usuários
+        onView(allOf(withId(R.id.lista_usuario_fab_adiciona),
+                isDisplayed())).perform(click());
 
         // clica no EditText e preenche com o nome do usuário
+        onView(allOf(withId(R.id.form_usuario_nome),
+                isDisplayed())).perform(click());
+
+        onView(allOf(withId(R.id.form_usuario_nome_edittext),
+                isDisplayed())).perform(replaceText("Douglas"), closeSoftKeyboard());
+
+        // clica em adicionar
+        onView(allOf(withId(android.R.id.button1), withText("Adicionar")))
+                .perform(scrollTo(), click());
+
+        // clica no back do android
+        pressBack();
+
+        // clica no fab lances do leilão
+        onView(withId(R.id.lances_leilao_fab_adiciona))
+                .perform(click());
+
+        // verifica visibilidade do dialog com o titulo esperado
+        onView(withText("Novo lance"))
+                .check(matches(isDisplayed()));
+
+        // clica no edit text de valor e presente
+        onView(withId(R.id.form_usuario_nome_edittext))
+                .perform(click(), replaceText("200"), closeSoftKeyboard());
+
+        // seleciona o usuario (spinner)
+        onView(withId(R.id.form_lance_usuario))
+                .perform(click());
+        onData(is(new Usuario(1, "Douglas")))
+                .inRoot(isPlatformPopup())
+                .perform(click());
+
+        // clica no botão propor
+        onView(withText("Propor"))
+                .perform(click());
+
+        // fazer assertion para as views de maior e menor lance, e tambem para os maiores lances
+        FormatadorDeMoeda formatador = new FormatadorDeMoeda();
+        onView(withId(R.id.lances_leilao_maior_lance))
+                .check(matches(allOf(withText(formatador.formata(200)), isDisplayed())));
+        onView(withId(R.id.lances_leilao_menor_lance))
+                .check(matches(allOf(withText(formatador.formata(200)), isDisplayed())));
+        onView(withId(R.id.lances_leilao_maiores_lances))
+                .check(matches(allOf(withText(formatador.formata(200) + " - (1) Douglas\n"), isDisplayed())));
+
+    }
+
+    @Test
+    public void deve_AtualizarLanceDoLeilao_QuandoReceberUmLance_Refatorado() throws IOException {
+
+        tentaSalvarLeilaoNaAPI(new Leilao("Carro"));
+
+        mainActivity.launchActivity(new Intent());
+
+        onView(withId(R.id.lista_leilao_recyclerview))
+                .perform(actionOnItemAtPosition(0, click()));
+
+        onView(allOf(withId(R.id.lances_leilao_fab_adiciona), isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withText("Usuários não encontrados"), withId(R.id.alertTitle)))
+                .check(matches(isDisplayed()));
+        onView(allOf(withText("Não existe usuários cadastrados! Cadastre um usuário para propor o lance."), withId(android.R.id.message)))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(withText("Cadastrar usuário"), isDisplayed())).perform(click());
+
+        onView(allOf(withId(R.id.lista_usuario_fab_adiciona),
+                isDisplayed())).perform(click());
+
+        onView(allOf(withId(R.id.form_usuario_nome_edittext),
+                isDisplayed())).perform(click(), typeText("Douglas"), closeSoftKeyboard());
+
+        onView(allOf(withId(android.R.id.button1), withText("Adicionar")))
+                .perform(scrollTo(), click());
+
+        pressBack();
+
+        onView(allOf(withId(R.id.lances_leilao_fab_adiciona), isDisplayed()))
+                .perform(click());
+
+        propoeNovoLance("200", 1, "Douglas");
+
+        FormatadorDeMoeda formatador = new FormatadorDeMoeda();
+        onView(withId(R.id.lances_leilao_maior_lance))
+                .check(matches(allOf(withText(formatador.formata(200)), isDisplayed())));
+        onView(withId(R.id.lances_leilao_menor_lance))
+                .check(matches(allOf(withText(formatador.formata(200)), isDisplayed())));
+        onView(withId(R.id.lances_leilao_maiores_lances))
+                .check(matches(allOf(withText(formatador.formata(200) + " - (1) Douglas\n"), isDisplayed())));
+
+    }
+
+    @Test
+    public void deve_AtualizarLancesDoLeilao_QuandoReceberTresLances_Refatorado2() throws IOException {
+
+        tentaSalvarLeilaoNaAPI(new Leilao("Carro"));
+
+        tentaSalvarUsuariosNoBancoDeDados(new Usuario("Douglas"), new Usuario("Fran"));
+
+        mainActivity.launchActivity(new Intent());
+
+        onView(withId(R.id.lista_leilao_recyclerview))
+                .perform(actionOnItemAtPosition(0, click()));
+
+        propoeNovoLance("200", 1, "Douglas");
+        propoeNovoLance("300", 2, "Fran");
+        propoeNovoLance("400", 1, "Douglas");
+
+        FormatadorDeMoeda formatador = new FormatadorDeMoeda();
+        onView(withId(R.id.lances_leilao_maior_lance))
+                .check(matches(allOf(withText(formatador.formata(400)), isDisplayed())));
+        onView(withId(R.id.lances_leilao_menor_lance))
+                .check(matches(allOf(withText(formatador.formata(200)), isDisplayed())));
+        onView(withId(R.id.lances_leilao_maiores_lances))
+                .check(matches(allOf(withText(formatador.formata(400) + " - (1) Douglas\n" +
+                        formatador.formata(200) + " - (2) Fran\n" +
+                        formatador.formata(400) + " - (1) Douglas\n"), isDisplayed())));
+
+    }
+
+    @Test
+    public void deve_AtualizarLancesDoLeilao_QuandoReceberUmLanceMuitoAlto() throws IOException {
+
+        tentaSalvarLeilaoNaAPI(new Leilao("Carro"));
+
+        tentaSalvarUsuariosNoBancoDeDados(new Usuario("Douglas"));
+
+        propoeNovoLance("2000000000", 1, "Douglas");
+
+        FormatadorDeMoeda formatador = new FormatadorDeMoeda();
+        onView(withId(R.id.lances_leilao_maior_lance))
+                .check(matches(allOf(withText(formatador.formata(2000000000)), isDisplayed())));
+        onView(withId(R.id.lances_leilao_menor_lance))
+                .check(matches(allOf(withText(formatador.formata(2000000000)), isDisplayed())));
+        onView(withId(R.id.lances_leilao_maiores_lances))
+                .check(matches(allOf(withText(formatador.formata(2000000000) + " - (1) Douglas\n"), isDisplayed())));
 
     }
 
